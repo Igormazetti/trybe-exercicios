bf0c6e6471f3f1e679a3a1b6404635fd88191554
Igor <iigormazetti@hotmail.com>
2021-11-26 15:14:43 -0300

    programa 05 exercicios javascript

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/programa05/index.js
@@ -0,0 +1,13 @@
+const firstAng = 35;
+const secondAng = 100;
+const thirdAng = "x";
+
+const soma = firstAng + secondAng + thirdAng;
+
+if (soma <= 180) {
+  console.log(true);
+} else if (soma > 180) {
+  console.log(false);
+} else {
+  console.log("Valor inválido");
+}
