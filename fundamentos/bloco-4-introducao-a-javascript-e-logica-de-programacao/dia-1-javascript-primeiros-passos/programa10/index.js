599ee53524c5259892d36e8fcd45f6ecf974b0ca
Igor <iigormazetti@hotmail.com>
2021-11-26 15:49:03 -0300

    programa 10 exercicios javascript

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/programa10/index.js
@@ -0,0 +1,11 @@
+const a = 10;
+const b = 15;
+
+const lucro = (b - a) * 1000;
+const imposto = (a * 1000) / 5;
+
+if (lucro - imposto >= 0) {
+  console.log(lucro - imposto);
+} else {
+  console.log("Valor inválido");
+}
