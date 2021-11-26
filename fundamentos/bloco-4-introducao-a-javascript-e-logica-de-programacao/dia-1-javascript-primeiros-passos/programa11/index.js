2c9f3ccc9ef327512bc6347d2557200094041cd2
Igor <iigormazetti@hotmail.com>
2021-11-26 19:37:05 -0300

    programa 11 correcao

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/programa11/index.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/programa11/index.js
@@ -1,29 +1,60 @@
-const salarioBruto = 2000.53;
+const salarioBruto = 3000.0;
 
 let salarioLiquido;
+let inss;
+let ir;
 
-switch (salarioBruto) {
+switch (true) {
   case salarioBruto <= 1556.94:
-    salarioLiquido = (salarioBruto / 100) * 8;
-    console.log(salarioLiquido);
+    inss = (salarioBruto / 100) * 8;
+    console.log(inss);
     break;
   case salarioBruto >= 1556.95 && salarioBruto <= 2594.92:
-    salarioLiquido = (salarioBruto / 100) * 9;
-    console.log(salarioLiquido);
+    inss = (salarioBruto / 100) * 9;
+    console.log(inss);
     break;
   case salarioBruto >= 2594.92 && salarioBruto <= 5189.82:
-    salarioLiquido = (salarioBruto / 100) * 11;
-    console.log(salarioLiquido);
+    inss = (salarioBruto / 100) * 11;
+    console.log(inss);
     break;
   case salarioBruto > 5189.83:
     if ((salarioBruto / 100) * 11 <= 570) {
-      salarioLiquido = (salarioBruto / 100) * 11;
+      inss = (salarioBruto / 100) * 11;
     } else {
-      salarioLiquido = 570;
+      inss = 570;
     }
-    console.log(salarioLiquido);
+    console.log(inss);
     break;
   default:
     console.log("algo deu errado");
     break;
 }
+
+switch (true) {
+  case salarioBruto <= 1903.98:
+    ir = 0;
+    console.log(ir);
+    break;
+  case salarioBruto >= 1903.99 && salarioBruto <= 2826.65:
+    ir = (salarioBruto / 100) * 7.5 - 142.8;
+    console.log(ir);
+    break;
+  case salarioBruto >= 2826.66 && salarioBruto <= 3751.05:
+    ir = (salarioBruto / 100) * 15 - 354.8;
+    console.log(ir);
+    break;
+  case salarioBruto >= 3751.06 && salarioBruto <= 4664.68:
+    ir = (salarioBruto / 100) * 22.5 - 636.13;
+    console.log(ir);
+    break;
+  case salarioBruto > 4664.68:
+    ir = (salarioBruto / 100) * 27.5 - 869.36;
+    console.log(ir);
+    break;
+  default:
+    console.log("algo deu errado no calculo do ir");
+    break;
+}
+
+salarioLiquido = salarioBruto - inss - ir;
+console.log(salarioLiquido);
