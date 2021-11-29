307af52bd774965d635a168fb0cab5a6ab3dd3ee
Igor <iigormazetti@hotmail.com>
2021-11-29 14:08:09 -0300

    exercicio 09 - array e loop for dividindo o conteudo por 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio08/index.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio08/index.js
@@ -4,3 +4,8 @@ for (let i = 1; i < 26; i++) {
   arr.push(i);
 }
 console.log(arr);
+
+for (let index = 0; index < arr.length; index++) {
+  const element = arr[index] / 2;
+  console.log(element);
+}
