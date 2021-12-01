58b9438af038dc8d50ea4727b6f51412c714c795
Igor <iigormazetti@hotmail.com>
2021-12-01 17:24:29 -0300

    exercicio 04 - descobrir o nome com mais caracteres

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio04/index.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio04/index.js
@@ -1,12 +1,14 @@
 function retornaMaior(arr) {
   let maior = 0;
+  let nome = "";
 
   for (let index in arr) {
     if (arr[index].length > maior) {
-      maior = arr[index];
+      maior = arr[index].length;
+      nome = arr[index];
     }
   }
-  console.log(maior);
+  console.log(nome);
 }
 
 retornaMaior(["José", "Lucas", "Nádia", "Fernanda", "Cairo", "Joana"]);
