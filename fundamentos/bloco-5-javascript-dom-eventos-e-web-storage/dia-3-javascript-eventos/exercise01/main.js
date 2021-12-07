54a3867f2b70bb621723ee3439f281381dc7fca7
Igor <iigormazetti@hotmail.com>
2021-12-07 12:12:27 -0300

    redirecionamento de pagina com dbclick

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercise01/main.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercise01/main.js
@@ -21,7 +21,7 @@ thirdLi.addEventListener("click", handleTech);
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
-function handleInput(event) {
+function handleInput() {
   const selectTech = document.querySelector(".tech");
   const input = document.getElementById("input");
 
@@ -32,6 +32,10 @@ input.addEventListener("keyup", handleInput);
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
+function handleSubtitle() {
+  location.href = "https://www.linkedin.com/in/igormazetti/";
+}
+myWebpage.addEventListener("dblclick", handleSubtitle);
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
