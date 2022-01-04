13fca46aef2bd8c8dedbf8368d9df6012cbc043d
Igor <iigormazetti@hotmail.com>
2022-01-04 14:31:36 -0300

    botao clear

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/curriculo/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/curriculo/script.js
@@ -44,6 +44,39 @@ handleStates();
 const sendButton = document.getElementById("send");
 function handleSubmit(event) {
   event.preventDefault();
-  const curriculo = document.createElement("div");
+
+  const name = document.getElementById("text-input");
+  if (name.value.length > 40 || name.value.length === 0) {
+    alert("Nome inválido");
+  }
 }
 sendButton.addEventListener("click", handleSubmit);
+
+const clearButton = document.getElementById("clear");
+function handleClear() {
+  const name = document.getElementById("text-input");
+  name.value = null;
+  const email = document.getElementById("email-input");
+  email.value = null;
+  const cpf = document.getElementById("cpf-input");
+  cpf.value = null;
+  const endereco = document.getElementById("endereço-input");
+  endereco.value = null;
+  const cidade = document.getElementById("cidade-input");
+  cidade.value = null;
+  const resumo = document.getElementById("resume");
+  resumo.value = null;
+
+  let estados = document.getElementById("list");
+  estados.value = null;
+
+  const cargo = document.getElementById("cargo-input");
+  cargo.value = null;
+  const descricao = document.getElementById("cargo-description");
+  descricao.value = null;
+
+  const data = document.getElementById("data");
+  data.value = null;
+}
+
+clearButton.addEventListener("click", handleClear);
