64718f5aea1c7038b7190fb4d855fbd9ad783187
Igor <iigormazetti@hotmail.com>
2022-01-05 08:48:47 -0300

    exemplos da aula

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/aula/library.js
@@ -0,0 +1,10 @@
+// let moment = require('moment');
+
+// let brasilTetra = moment("17071994", "DDMMYYYY");
+// let fromNow = brasilTetra.fromNow();
+
+// console.log('Ouvimos o "É TETRAAAA!!" há ' + fromNow);
+
+cheet("↑ ↑ ↓ ↓ ← → ← → b a", function () {
+  alert("Voilà!");
+});
