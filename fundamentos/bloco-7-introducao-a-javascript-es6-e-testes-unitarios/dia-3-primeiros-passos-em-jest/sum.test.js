b3d27ce291498714fdd1f0a95d248d30b50793ae
Igor <iigormazetti@hotmail.com>
2022-01-17 10:34:08 -0300

    exercicio 02 function myRemove tests

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -1,25 +1,31 @@
-const sum = require("./sum");
+// const sum = require("./sum");
 
-describe("sum tests", () => {
-  test("sums two values", () => {
-    expect(sum(4, 5)).toBe(9);
-  });
+// describe("sum tests", () => {
+//   test("sums two values", () => {
+//     expect(sum(4, 5)).toBe(9);
+//   });
 
-  test("sums two values", () => {
-    expect(sum(0, 0)).toBe(0);
-  });
+//   test("sums two values", () => {
+//     expect(sum(0, 0)).toBe(0);
+//   });
 
-  test("throws error", () => {
-    expect(() => {
-      sum(4, "5");
-    }).toThrow();
-  });
-});
-
-// const myRemove = require("./sum");
-
-// describe("remove funtion tests", () => {
-//   it("test if its working", () => {
-//     expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+//   test("throws error", () => {
+//     expect(() => {
+//       sum(4, "5");
+//     }).toThrow();
 //   });
 // });
+
+const myRemove = require("./sum");
+
+describe("remove funtion tests", () => {
+  it("test if its working", () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+  });
+  it("test if its working with matching not", () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
+  it("test if its working with matching not", () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
+});
