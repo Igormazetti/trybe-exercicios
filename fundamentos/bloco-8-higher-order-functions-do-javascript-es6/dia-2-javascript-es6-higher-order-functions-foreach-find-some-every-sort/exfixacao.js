b87863b4d8d9ce40326f175cc21b887a70fa09dc
Igor <iigormazetti@hotmail.com>
2022-01-19 14:32:29 -0300

    exercicio 02 de fixacao com find

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exfixacao.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exfixacao.js
@@ -7,3 +7,13 @@ const findDivisibleBy3And5 = () => {
 };
 
 console.log(findDivisibleBy3And5());
+
+// 2 - Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista:
+
+const names = ["João", "Irene", "Fernando", "Maria"];
+
+const findNameWithFiveLetters = () => {
+  return names.find((name) => name.length === 5);
+};
+
+console.log(findNameWithFiveLetters());
